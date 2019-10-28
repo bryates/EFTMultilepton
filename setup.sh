411a7556ff8b274e764950394bc436abe9dd3d44
Andrew42 <awightma@nd.edu>
2019-10-28 16:27:11 -0400

    Fix typo in git clone commands

--- setup.sh
+++ setup.sh
@@ -21,9 +21,9 @@ EOF
 
    git cms-init > /dev/null
 
-   git git@github.com:cms-govner/MiniAOD.git
-   git git@github.com:cms-govner/ttH-LeptonID.git ttH/LeptonID
-   git git@github.com:cms-govner/EFTMultilepton.git
+   git clone git@github.com:cms-govner/MiniAOD.git
+   git clone git@github.com:cms-govner/ttH-LeptonID.git ttH/LeptonID
+   git clone -b cmssw_9_4_10 git@github.com:cms-govner/EFTMultilepton.git
    echo "NOTE: If setup.sh is ever updated to CMSSW_9_4_13 or higher, the following command can be omitted."
    cp -r /afs/crc.nd.edu/user/g/gsmith15/Public/additional_code_for_EFT/L1Prefiring . ## Note: if not running this on earth, change to scp -r
 
