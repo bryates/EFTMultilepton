3609e0dee3ddc4dc65673213416c490e3de859fa
Andrew42 <awightma@nd.edu>
2019-11-21 20:09:17 -0500

    Add back in count variable

--- TemplateMakers/test/MakeGoodPlot2/run.h
+++ TemplateMakers/test/MakeGoodPlot2/run.h
@@ -3,9 +3,11 @@ void HistMaker::run(TTreeReader & newreader, int firstevent, int lastevent)
 {
     if (firstevent>-1 || lastevent>-1) newreader.SetEntriesRange(firstevent,lastevent);
     cout << "Starting event loop..." << endl;
+    int count = 0;
     event_counter = 0;
     while (newreader.Next()) {
         doOneEvent();
+        count++;
         // if (event_counter > 100) break;
         if (count%5000==0) cout << "Event: " << count << endl;
     }
